1fcad9823cfc5a6e63d6cb8d3bba47968906cde4
Camila Bandeira <88980333+bandeiramila@users.noreply.github.com>
2023-11-19 22:17:13 -0300

    new object

---
+++ app/src/main/java/com/example/projetofinal/ProductsBudget.java
@@ -0,0 +1,51 @@
+package com.example.projetofinal;
+
+import java.io.Serializable;
+
+public class ProductsBudget implements Serializable {
+    int id;
+    int id_orcamento;
+    int id_produto;
+    int quantidade;
+    double valor_unitario;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_orcamento() {
+        return id_orcamento;
+    }
+
+    public void setId_orcamento(int id_orcamento) {
+        this.id_orcamento = id_orcamento;
+    }
+
+    public int getId_produto() {
+        return id_produto;
+    }
+
+    public void setId_produto(int id_produto) {
+        this.id_produto = id_produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValor_unitario() {
+        return valor_unitario;
+    }
+
+    public void setValor_unitario(double valor_unitario) {
+        this.valor_unitario = valor_unitario;
+    }
+}
